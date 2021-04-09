a6599badd7568cc9b27f75c9328a341ca70ebbdc
paulham98 <pauljungho.ham@gmail.com>
2021-04-09 21:28:40 +0900

    2021_04_09

--- tutorialApp/src/S_Car.java
+++ tutorialApp/src/S_Car.java
@@ -1,24 +1,27 @@
 import java.util.Scanner;
 class vCar{
-    protected int speed;
+    protected int speed = 0;
     protected String name;
-    protected int maxSpeed;
+    protected int maxSpeed = 0;
     protected String owner;
+    protected int cost = 0;
+    protected String costPf;
 
     vCar(String name, String owner){
         this.name = name;
         this.owner = owner;
-        System.out.println("자동차 이름:"+name+" 차주 이름: "+owner+"\n");
+        System.out.println("자동차 이름:"+name+" 차주 이름: "+owner);
         System.out.println("속도와 제한속도를 설정해주세요");
     }
     vCar(String name){
         this.name = name;
-        System.out.println("자동차 이름:"+name+"\n");
+        System.out.println("자동차 이름:"+name);
         System.out.println("차주이름과 속도, 제한속도를 설정해주세요");
     }
     public void show(){
-        System.out.println(this.owner +"님의 자동차("+ name +") 현재속도는"+this.speed+
-                ", 제한속도는"+this.maxSpeed+"입니다.");
+        System.out.println(this.owner +"님의 자동차("+ name +") 현재속도는 "+this.speed+
+                "km, 제한속도는 "+this.maxSpeed+"km 입니다.");
+        System.out.println("가격: "+cost+"만원 , 연비: "+costPf);
     }
 
     void speedUp(int a){
@@ -43,10 +46,15 @@ class vCar{
 }
 public class S_Car {
     public static void main(String[] args) {
-        vCar a = new vCar("람보르기니", "임준서");
-        vCar b = new vCar("페라리", "함정호");
+        vCar a = new vCar("2020 아반떼", "임준서");
+        vCar b = new vCar("2021 테슬라 모델 Y", "함정호");
         a.speed = 40;
+        a.costPf = "14.4km/L";
+        a.cost = 1570;
+
         b.speed = 50;
+        b.costPf = "4.8km/kWh";
+        b.cost = 6000;
         a.show();
         b.show();
     }
